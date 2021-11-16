f1bb78d33fd2c3d84cb8152a0565ff9be2092bcf
dodkirua <pybouttefeux@gmail.com>
2021-11-16 14:56:51 +0100

    css finis

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     entry : {
@@ -7,7 +9,39 @@ module.exports = {
         admin: './assets/admin.js'
     },
     output: {
-        path: path.resolve(__dirname, 'public/assets/'),
-        filename: "[name]-bundle.js"
-    }
-}
+        path: path.resolve(__dirname, 'public/build/js'),
+        filename: "[name].js"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        // style-loader for css in head
+                        // MiniCssExtractPlugin for css in a same name file
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            import: true,
+                            url: true,
+                            sourceMap: true,
+                        }
+                    },
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+
+    plugins: [].concat([new MiniCssExtractPlugin({
+            filename: "../css/[name].css"
+    })]),
+};
