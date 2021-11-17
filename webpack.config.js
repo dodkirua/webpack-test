4edce889929d5bb21bda531d168d979d4bce7d10
dodkirua <pybouttefeux@gmail.com>
2021-11-17 17:05:39 +0100

    comit du soir

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,54 @@
-const webpack = require('webpack');
+require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    entry : {
+    entry: {
         front: './assets/front.js',
         admin: './assets/admin.js'
     },
     output: {
-        path: path.resolve(__dirname, 'public/build/js'),
-        filename: "[name].js"
+        path: path.resolve(__dirname, 'public/build'),
+        filename: "[name].js",
+        publicPath: "build",
     },
 
     module: {
         rules: [
+            // rules css files
             {
                 test: /\.css$/i,
                 use: [
                     {
-                        // style-loader for css in head
-                        // MiniCssExtractPlugin for css in a same name file
+// style-loader for css in head
+// MiniCssExtractPlugin for css in a same name file
                         loader: MiniCssExtractPlugin.loader,
                     },
                     {
                         loader: "css-loader",
                         options: {
-                            import: true,
-                            url: true,
                             sourceMap: true,
                         }
                     },
                 ],
+            },
+            //rules images files
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/ressource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+            },
+
+            //babel config
+            {
+                test: /\.js$/i,
+                loader: "babel-loader",
+                options: {
+                    presets: ['@']
+                }
             },
         ],
     },
@@ -42,6 +59,6 @@ module.exports = {
     },
 
     plugins: [].concat([new MiniCssExtractPlugin({
-            filename: "../css/[name].css"
+        filename: "../css/[name].css"
     })]),
 };
