6f13813d0daeb513e2112645d621ed25fa6107d7
dodkirua <pybouttefeux@gmail.com>
2021-11-18 11:42:56 +0100

    commit terser

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'public/build'),
-        filename: "[name].js",
+        filename: "js/[name].js",
         publicPath: "build",
         clean: true,
     },
